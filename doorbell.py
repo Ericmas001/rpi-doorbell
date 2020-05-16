2c3166b88e4d3f60a8615a6041e57060fa644a1b
Éric Massé <Ericmas001@gmail.com>
2020-05-16 19:55:53 -0400

    re-adding first buzzer code

--- doorbell.py
+++ doorbell.py
@@ -9,8 +9,27 @@ GPIO.setup(BUTTON, GPIO.IN)
 GPIO.setup(BUZZER, GPIO.OUT)
 
 GPIO.output(BUZZER, GPIO.LOW)
-buzzer = GPIO.PWM(BUZZER, 2000)
-buzzer.start(0)
+
+def buzz(noteFreq, duration):
+    halveWaveTime = 1 / (noteFreq * 2 )
+    waves = int(duration * noteFreq)
+    for i in range(waves):
+       GPIO.output(BUZZER, GPIO.HIGH)
+       sleep(halveWaveTime)
+       GPIO.output(BUZZER, GPIO.LOW)
+       sleep(halveWaveTime)
+
+def play():
+    t=0
+    notes=[262,294,330,262,262,294,330,262,330,349,392,330,349,392,392,440,392,349,330,262,392,440,392,349,330,262,262,196,262,262,196,262]
+    duration=[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1,0.5,0.5,1,0.25,0.25,0.25,0.25,0.5,0.5,0.25,0.25,0.25,0.25,0.5,0.5,0.5,0.5,1,0.5,0.5,1]
+    for n in notes:
+        buzz(n, duration[t])
+        sleep(duration[t] *0.1)
+        t+=1
+
+#buzzer = GPIO.PWM(BUZZER, 2000)
+#buzzer.start(0)
 
 '''
 How melodies are transposed into code that is played on buzzer:
@@ -125,17 +144,15 @@ try:
 	while True:
 		if GPIO.input(BUTTON):
 			print("Someone is ringing !!!")
-#			playSong(doorbell_notes,doorbell_beats,0.001)
-#			playSong(doorbell_notes,doorbell_beats,0.01)
-			playSong(doorbell_notes,doorbell_beats,0.2)
-#			sleep(0.5)
-#			playSong(doorbell2_notes,doorbell2_beats,0.5)
+#			playSong(doorbell_notes,doorbell_beats,0.2)
+			#sleep(0.5)
+			#playSong(doorbell2_notes,doorbell2_beats,0.5)
 			#sleep(0.5)
 			#playSong(starwars_notes, starwars_beats, 0.2)
 			#sleep(0.5)
 			#playSong(londonbridges_notes, londonbridges_beats, 0.3)
 			#sleep(0.5)
-			#play()
+			play()
 			#GPIO.output(BUZZER, GPIO.HIGH)
 			#sleep(0.5)
 			#GPIO.output(BUZZER, GPIO.LOW)
