c1daed00ea20980dfc20d351c6924135d82e45a4
Éric Massé <Ericmas001@gmail.com>
2020-05-16 17:08:45 -0400

    melody

--- doorbell.py
+++ doorbell.py
@@ -11,14 +11,33 @@ GPIO.setup(BUZZER, GPIO.OUT)
 
 GPIO.output(BUZZER, GPIO.LOW)
 
+def buzz(noteFreq, duration):
+    halveWaveTime = 1 / (noteFreq * 2 )
+    waves = int(duration * noteFreq)
+    for i in range(waves):
+       GPIO.output(BUZZER, True)
+       sleep(halveWaveTime)
+       GPIO.output(BUZZER, False)
+       sleep(halveWaveTime)
+
+def play():
+    t=0
+    notes=[262,294,330,262,262,294,330,262,330,349,392,330,349,392,392,440,392,349,330,262,392,440,392,349,330,262,262,196,262,262,196,262]
+    duration=[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1,0.5,0.5,1,0.25,0.25,0.25,0.25,0.5,0.5,0.25,0.25,0.25,0.25,0.5,0.5,0.5,0.5,1,0.5,0.5,1]
+    for n in notes:
+        buzz(n, duration[t])
+        time.sleep(duration[t] *0.1)
+        t+=1	
+	
 try:
 	while True:
 		if GPIO.input(BUTTON):
 			print("Someone is ringing !!!")
-			GPIO.output(BUZZER, GPIO.HIGH)
-			sleep(0.5)
-			GPIO.output(BUZZER, GPIO.LOW)
-			sleep(1.5)
+			play()
+			#GPIO.output(BUZZER, GPIO.HIGH)
+			#sleep(0.5)
+			#GPIO.output(BUZZER, GPIO.LOW)
+			#sleep(1.5)
 		else:
 			sleep(0.01)
 finally:
